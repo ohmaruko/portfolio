e74992afeb8f517d744292fb35ef5c7e82e654fd
madoka <mnogaki@my.bcit.ca>
2025-01-07 22:46:57 -0800

    update GlowKombu content

--- pages/works/GlowKombu/index.js
+++ pages/works/GlowKombu/index.js
@@ -21,6 +21,8 @@ export default function GlowKombu() {
                 <p>November 2024</p>
                 <div className={styles.mainImage} >
                     <img src='/images/glowkombu/glowkombu_straight.png' alt='GlowKombu Kombucha'/>
+                    <p>GLOW KOMBU’s packaging design invites consumers to feel warmth, balance, and vitality glowing from the relaxed sun the moment they pick up the can. The sun's rays glow around the can, creating a luminous and inviting aesthetic that embodies the natural energy of kombucha.</p>
+                    <p>The design uses a monotone color palette for a clean, minimalistic feel, with a distinct color assigned to each flavor. These colors are thoughtfully chosen to reflect the essence of the flavor while maintaining an organic and earthy tone. Every detail of the design is tailored to resonate with health-conscious consumers, making them feel grounded, refreshed, and connected to the natural vitality.</p>
                 </div>
                 <div className={styles.frontSide}>
                     <img src='/images/glowkombu/mockup_face_ginger.png' alt='GlowKombu Kombucha'/>
