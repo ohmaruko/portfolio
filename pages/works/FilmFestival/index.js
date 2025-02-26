0cff7ab8cac3b755016d5a066a94fa7393858b8d
madoka <mnogaki@my.bcit.ca>
2025-02-25 17:11:13 -0800

    update Filmfestival images

--- pages/works/FilmFestival/index.js
+++ pages/works/FilmFestival/index.js
@@ -20,13 +20,16 @@ export default function Works() {
                     <p>February 2024</p>
                     <div className={styles.filmAdsContainer}>
                       <div className={styles.filmAds}>
-                        <img src='/images/film1.png' alt='Hair Sunset Ad1' />
+                        <img src='/images/filmfestival/film01.png' alt='Hair Sunset Ad1' />
                       </div>
                       <div className={styles.filmAds}>
-                        <img src='/images/film2.png' alt='Hair Sunset Ad2' />
+                        <img src='/images/filmfestival/film02.png' alt='Hair Sunset Ad2' />
+                      </div>
+                      <div className={styles.filmAds}>
+                        <img src='/images/filmfestival/film03.png' alt='Hair Sunset Ad2' />
                       </div>
                     </div>
-                    <p>Designed two variations of typography layout for a film festival.</p>
+                    <p>Designed three variations of typography layout for a film festival.</p>
                     <p><strong>Fonts</strong>: <a href="https://fonts.google.com/specimen/Rubik" target='_blank'>Rubik</a>,
                     <a href="https://fonts.google.com/specimen/Bitter" target='_blank'> Bitter</a></p>
                     <p><strong>Tools</strong>: Adobe Illustrator, Grid layout</p>
